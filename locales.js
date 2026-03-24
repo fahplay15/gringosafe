7bf3302ef880365d351387f55ae6076790031078
fahplay15 <95629346+fahplay15@users.noreply.github.com>
2026-03-24 10:04:46 -0300

    Delete locales.js

--- locales.js
+++
@@ -1,7 +0,0 @@
-// Exemplo de dicionário de idiomas
-const dicionario = {
-  pt: { splashSub: "Preço Justo em Qualquer Lugar" },
-  en: { splashSub: "Fair Price Anywhere" },
-  es: { splashSub: "Precio Justo en Cualquier Lugar" }
-};
-export default dicionario;
